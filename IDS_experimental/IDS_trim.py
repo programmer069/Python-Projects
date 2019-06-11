d53515a76d582a3284b8cfde2a73c13c6a7e6b8a
vinay <reddyvinay69@gmail.com>
2019-06-12 01:22:26 +0530

    modified datasets, added target columns

--- IDS_experimental/IDS_trim.py
+++ IDS_experimental/IDS_trim.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 ## columns 0,1,4,5,23,35 are selected
 
-df = pd.read_csv("/home/vinay/Downloads/Network-Intrusion-Detection-master/NSL-KDD/kddtest.csv", header=None)
+df = pd.read_csv("/home/vinay/Downloads/Network-Intrusion-Detection-master/NSL-KDD/kddtrain.csv", header=None)
 
 print(df.head())
 
-columnsList = [0,1,4,5,23,35]
+columnsList = [0,1,4,5,23,35,41,42]
 df_mod = df.iloc[:,columnsList]
 print(df_mod.head())
 
-col_names = ["duration","protocolType","srcBytes","dstBytes","srvCount","dst_host_same_src_port_rate"]
+col_names = ["duration","protocolType","srcBytes","dstBytes","srvCount","dst_host_same_src_port_rate","attackType","attackNumber"]
 
 df_mod.columns = col_names
 
-df_mod.to_csv("kddtest_trim.csv")
+df_mod.to_csv("kddtrain_trim.csv")
 
 #print(df.cloumns)
